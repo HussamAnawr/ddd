88cf01c2ed1cc5d99222a1bb190edc3f8b59bf93
Hussam <o.hussam@gmail.com>
2023-05-23 21:53:40 +0300

    repo-f

--- repository.py
+++ repository.py
@@ -1,10 +1,24 @@
 import model
 import abc 
 
-class  AbstractRepository(abc.ABC):
+class AbstractRepository(abc.ABC):
     @abc.abstractmethod
     def add(self, batch: model.Batch):
         raise NotImplementedError
     
     def get(self, reference) -> model.Batch:
         raise NotImplementedError
+    
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch: model.Batch):
+        self.session.add(batch)
+
+    def get(self, reference) -> model.Batch:
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+    
+    def list(self):
+        return self.session.query(model.Batch).all()
+    
